e1c71839c500ffde894e7f42fe7c2324bce86143
Alain Perkaz <alain.perkaz@sennder.com>
2019-12-11 16:38:22 +0100

    Update dist

--- dist/index.js
+++ dist/index.js
@@ -35,7 +35,7 @@ module.exports =
 /******/ 	// the startup function
 /******/ 	function startup() {
 /******/ 		// Load entry module and return exports
-/******/ 		return __webpack_require__(454);
+/******/ 		return __webpack_require__(132);
 /******/ 	};
 /******/
 /******/ 	// run startup
@@ -44,7 +44,88 @@ module.exports =
 /************************************************************************/
 /******/ ({
 
-/***/ 44:
+/***/ 132:
+/***/ (function(__unusedmodule, __unusedexports, __webpack_require__) {
+
+
+const core = __webpack_require__(286);
+const { context, GitHub } = __webpack_require__(919);
+
+async function run() {
+  const trigger = core.getInput("trigger");
+  if (!trigger) {
+    core.setFailed("No `trigger` input given, aborting.");
+    return;
+  }
+  const reaction = core.getInput("reaction");
+  const { GITHUB_TOKEN } = process.env;
+  if (reaction && !GITHUB_TOKEN) {
+    core.setFailed('If "reaction" is supplied, GITHUB_TOKEN is required');
+    return;
+  }
+
+  if (
+    context.eventName === "issue_comment" &&
+    !context.payload.issue.pull_request
+  ) {
+    // not a pull-request comment, aborting
+    core.setOutput("triggered", "false");
+    return;
+  }
+
+  const { owner, repo } = context.repo;
+
+  const body =
+    context.eventName === "issue_comment"
+      ? context.payload.comment.body
+      : context.payload.pull_request.body;
+
+  if (!body.includes(trigger)) {
+    core.setOutput("triggered", "false");
+    return;
+  }
+
+  console.log("hi");
+  console.log(context);
+  const time = new Date().toTimeString();
+  core.setOutput("time", time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  console.log(`The event payload: ${payload}`);
+
+  core.setOutput("triggered", "true");
+
+  if (!reaction) {
+    return;
+  }
+
+  const client = new GitHub(GITHUB_TOKEN);
+  if (context.eventName === "issue_comment") {
+    await client.reactions.createForIssueComment({
+      owner,
+      repo,
+      comment_id: context.payload.comment.id,
+      content: reaction
+    });
+  } else {
+    await client.reactions.createForIssue({
+      owner,
+      repo,
+      issue_number: context.payload.pull_request.number,
+      content: reaction
+    });
+  }
+}
+
+run().catch(err => {
+  console.error(err);
+  core.setFailed("Unexpected error");
+});
+
+
+/***/ }),
+
+/***/ 286:
 /***/ (function() {
 
 eval("require")("@actions/core");
@@ -52,92 +133,7 @@ eval("require")("@actions/core");
 
 /***/ }),
 
-/***/ 454:
-/***/ (function(__unusedmodule, __unusedexports, __webpack_require__) {
-
-
-const core = __webpack_require__(44);
-const { context, GitHub } = __webpack_require__(840);
-
-async function run() {
-    core.setFailed(context);
-    return
-    
-    const trigger = core.getInput("trigger");
-    if (!trigger) {
-        core.setFailed("No `trigger` input given, aborting.");
-        return;
-    }
-    const reaction = core.getInput("reaction");
-    const { GITHUB_TOKEN } = process.env;
-    if (reaction && !GITHUB_TOKEN) {
-        core.setFailed('If "reaction" is supplied, GITHUB_TOKEN is required');
-        return;
-    }
-
-    if (
-        context.eventName === "issue_comment" &&
-        !context.payload.issue.pull_request
-    ) {
-        // not a pull-request comment, aborting
-        core.setOutput("triggered", "false");
-        return;
-    }
-
-    const { owner, repo } = context.repo;
-
-    const body =
-        context.eventName === "issue_comment"
-            ? context.payload.comment.body
-            : context.payload.pull_request.body;
-
-    if (!body.includes(trigger)) {
-        core.setOutput("triggered", "false");
-        return;
-    }
-
-    console.log('hi');
-    console.log(context)
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-      // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-    
-    
-    core.setOutput("triggered", "true");
-    
-    if (!reaction) {
-        return;
-    }
-
-    const client = new GitHub(GITHUB_TOKEN);
-    if (context.eventName === "issue_comment") {
-        await client.reactions.createForIssueComment({
-            owner,
-            repo,
-            comment_id: context.payload.comment.id,
-            content: reaction
-        });
-    } else {
-        await client.reactions.createForIssue({
-            owner,
-            repo,
-            issue_number: context.payload.pull_request.number,
-            content: reaction
-        });
-    }
-}
-
-run().catch(err => {
-    console.error(err);
-    core.setFailed("Unexpected error");
-});
-
-
-/***/ }),
-
-/***/ 840:
+/***/ 919:
 /***/ (function() {
 
 eval("require")("@actions/github");
--- index.js
+++ index.js
@@ -4,76 +4,72 @@ const core = require("@actions/core");
 const { context, GitHub } = require("@actions/github");
 
 async function run() {
-    core.setFailed(context);
-    return
-    
-    const trigger = core.getInput("trigger");
-    if (!trigger) {
-        core.setFailed("No `trigger` input given, aborting.");
-        return;
-    }
-    const reaction = core.getInput("reaction");
-    const { GITHUB_TOKEN } = process.env;
-    if (reaction && !GITHUB_TOKEN) {
-        core.setFailed('If "reaction" is supplied, GITHUB_TOKEN is required');
-        return;
-    }
+  const trigger = core.getInput("trigger");
+  if (!trigger) {
+    core.setFailed("No `trigger` input given, aborting.");
+    return;
+  }
+  const reaction = core.getInput("reaction");
+  const { GITHUB_TOKEN } = process.env;
+  if (reaction && !GITHUB_TOKEN) {
+    core.setFailed('If "reaction" is supplied, GITHUB_TOKEN is required');
+    return;
+  }
 
-    if (
-        context.eventName === "issue_comment" &&
-        !context.payload.issue.pull_request
-    ) {
-        // not a pull-request comment, aborting
-        core.setOutput("triggered", "false");
-        return;
-    }
+  if (
+    context.eventName === "issue_comment" &&
+    !context.payload.issue.pull_request
+  ) {
+    // not a pull-request comment, aborting
+    core.setOutput("triggered", "false");
+    return;
+  }
 
-    const { owner, repo } = context.repo;
+  const { owner, repo } = context.repo;
 
-    const body =
-        context.eventName === "issue_comment"
-            ? context.payload.comment.body
-            : context.payload.pull_request.body;
+  const body =
+    context.eventName === "issue_comment"
+      ? context.payload.comment.body
+      : context.payload.pull_request.body;
 
-    if (!body.includes(trigger)) {
-        core.setOutput("triggered", "false");
-        return;
-    }
+  if (!body.includes(trigger)) {
+    core.setOutput("triggered", "false");
+    return;
+  }
 
-    console.log('hi');
-    console.log(context)
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-      // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-    
-    
-    core.setOutput("triggered", "true");
-    
-    if (!reaction) {
-        return;
-    }
+  console.log("hi");
+  console.log(context);
+  const time = new Date().toTimeString();
+  core.setOutput("time", time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  console.log(`The event payload: ${payload}`);
 
-    const client = new GitHub(GITHUB_TOKEN);
-    if (context.eventName === "issue_comment") {
-        await client.reactions.createForIssueComment({
-            owner,
-            repo,
-            comment_id: context.payload.comment.id,
-            content: reaction
-        });
-    } else {
-        await client.reactions.createForIssue({
-            owner,
-            repo,
-            issue_number: context.payload.pull_request.number,
-            content: reaction
-        });
-    }
+  core.setOutput("triggered", "true");
+
+  if (!reaction) {
+    return;
+  }
+
+  const client = new GitHub(GITHUB_TOKEN);
+  if (context.eventName === "issue_comment") {
+    await client.reactions.createForIssueComment({
+      owner,
+      repo,
+      comment_id: context.payload.comment.id,
+      content: reaction
+    });
+  } else {
+    await client.reactions.createForIssue({
+      owner,
+      repo,
+      issue_number: context.payload.pull_request.number,
+      content: reaction
+    });
+  }
 }
 
 run().catch(err => {
-    console.error(err);
-    core.setFailed("Unexpected error");
+  console.error(err);
+  core.setFailed("Unexpected error");
 });
