f8cecb2373fb9c64bb3b5711ee77834f6490a0a2
Alain Perkaz <alain.perkaz@sennder.com>
2019-12-11 16:44:16 +0100

    Update dist

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ async function run() {
   }
 
   console.log("hi");
-  console.log(context);
+  console.log(context.payload.issue.pull_request);
   const time = new Date().toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
